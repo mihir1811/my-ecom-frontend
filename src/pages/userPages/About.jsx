3ef9d7568e0025f57d728a2fd657f229c77afa94
mihir Sabhaya <mihir.roxontechnologies@gmail.com>
2024-06-12 12:39:00 +0530

    format code

--- src/pages/userPages/About.jsx
+++ src/pages/userPages/About.jsx
@@ -3,12 +3,13 @@ import Header from "../../components/Header";
 import Footer2 from "../../components/footer2";
 
 const About = () => {
-  return <>
-  
-  <Header />
-  <h1>about page content</h1>
-  <Footer2 />
-  </>;
+  return (
+    <>
+      <Header />
+      <h1>about page content</h1>
+      <Footer2 />
+    </>
+  );
 };
 
 export default About;
--- src/pages/userPages/CheckOutPage.jsx
+++ src/pages/userPages/CheckOutPage.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Header from '../../components/Header'
-import Footer2 from '../../components/footer2'
+import React from "react";
+import Header from "../../components/Header";
+import Footer2 from "../../components/footer2";
 
 const CheckOutPage = () => {
   return (
     <>
-        <Header />
-
-        <h1>checkout content</h1>
-        <Footer2 />
+      <Header />
+      <h1>checkout content</h1>
+      <Footer2 />
     </>
-  )
-}
+  );
+};
 
-export default CheckOutPage
+export default CheckOutPage;
